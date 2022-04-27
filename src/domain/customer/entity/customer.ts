777880dde373555cac05be67f9abb5967b2a22a1
kevin farias <kevin.farias.dev@gmail.com>
2022-04-27 12:44:56 -0300

    feat: adding notification pattern in customer domain

--- src/domain/customer/entity/customer.ts
+++ src/domain/customer/entity/customer.ts
@@ -2,18 +2,24 @@ import CustomerAddressChangedEvent from "../event/customer-address-changed.event
 import CustomerCreatedEvent from "../event/customer-created.event";
 import eventDispatcher from "../../@shared/eventDispatcher";
 import Address from "../value-object/address";
+import Entity from "../../@shared/entity/entity.abstract";
+import NotificationError from "../../@shared/notification/notification.error";
 
-export default class Customer {
-    private _id: string;
+export default class Customer extends Entity {
     private _name: string;
     private _address!: Address;
     private _active: boolean = false;
     private _rewardPoints: number = 0;
 
     constructor(id: string, name: string) {
+        super();
         this._id = id;
         this._name = name;
         this.validate();
+
+        if (this.notification.hasErrors()) {
+            throw new NotificationError(this.notification.getErrors());
+        }
 
         const customerCreatedEvent = new CustomerCreatedEvent({
             id,
@@ -23,17 +29,19 @@ export default class Customer {
         eventDispatcher.notify(customerCreatedEvent);
     }
 
-    get id(): string {
-        return this._id;
-    }
-
     validate() {
-        if (this.name.length == 0) {
-            throw new Error("Name is required");
-        }
-
         if (this._id.length === 0) {
-            throw new Error("Id is required");
+            this.notification.addError({
+                context: "customer",
+                message: "Id is required"
+            });
+        }
+        
+        if (this.name.length == 0) {
+            this.notification.addError({
+                context: "customer",
+                message: "Name is required"
+            });
         }
     }
 
